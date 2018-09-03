b55eaa603ab48513a655387a49e082c58d7b5a92
Oleh1033 <oleg103356@gmail.com>
2018-09-03 15:20:43 +0200

    connect few files

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById("analys").addEventListener("click", function () {
+    document.querySelector('.bg-modal').style.display = 'flex';
+});
+document.querySelector('.close').addEventListener('click', function () {
+    document.querySelector('.bg-modal').style.display = 'none';
+});
+
+document.getElementById("addICO").addEventListener("click", function () {
+    document.querySelector('.bg-modal2').style.display = 'flex';
+});
+document.querySelector('.close2').addEventListener('click', function () {
+    document.querySelector('.bg-modal2').style.display = 'none';
+});
